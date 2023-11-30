60a42f68c3bcd8f1d3d6abb2d3cdc92c97c480b4
GabEd21 <cegdumandan@gmail.com>
2023-11-30 20:23:23 +0800

    chnage

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -2,15 +2,22 @@
 
 
 import posts from "@/utils/posts";
+import { PrismaClient } from "@prisma/client";
 
 export async function GET(request){
-    const data = posts
+    const prisma = new PrismaClient()
+    const data = await prisma.post.findMany()
+
     return new Response(JSON.stringify(data))
 }
 
 export async function POST(request){
     const data = await request.json()
-    
-    return new Response(JSON.stringify("Hey"))
+    console.log(data)
+    return new Response(
+        JSON.stringify(
+            {message: "Post succesfully added"}
+        )
+    )
 }
 
